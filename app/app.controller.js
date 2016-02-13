6afdd09f474cbbf668b3174cdfb1f997a20c000e
k_zenchyk <k_zenchyk@wargaming.net>
2016-02-13 21:27:23 +0300

    + add simple inputs

--- app/app.controller.js
+++ app/app.controller.js
@@ -5,24 +5,21 @@
   angular.module('gpApp')
     .controller('socialController', ['$scope', 'socialService', function($scope, socialService) {
 
-        socialService.getUsers().then(successCallback, errorCallback);
-
         $scope.selectedUser = {name:'', age:'', userpic: '', gender: ''};
         var userID = 1;
+
+        socialService.getUsers().then(successCallback, errorCallback);
+
         function successCallback(response){
-          var user = response.data[userID];
+          $scope.selectedUser = response.data[userID];
           console.log('USERS', $scope.selectedUser);
-          $scope.selectedUser = user;
+
         }
         function errorCallback(response){
           return  "Error: " + response.status + " " + response.statusText;
         }
 
-        //$scope.allusers = socialService.getUsers();
-
-        //$scope.all = $scope.allusers.data;
 
 
-        //console.log('ALL', $scope.all);
       }]);
 
