ca4c266c425e28521d0f62d6e4a9f542711cb1ee
Avinar-24 <art.gospodarsky@gmail.com>
2016-02-14 12:32:25 +0300

    Setting button Save and update guide

--- app/app.controller.js
+++ app/app.controller.js
@@ -1,25 +1,36 @@
 ;
-
+(function() {
   'use strict';
 
   angular.module('gpApp')
     .controller('socialController', ['$scope', 'socialService', function($scope, socialService) {
 
-        $scope.selectedUser = {name:'', age:'', userpic: '', gender: ''};
-        var userID = 1;
+      $scope.selectedUser = {
+        name: '',
+        age: '',
+        userpic: '',
+        gender: ''
+      };
 
-        socialService.getUsers().then(successCallback, errorCallback);
+      socialService.getUsers().then(successCallback, errorCallback);
 
-        function successCallback(response){
-          $scope.selectedUser = response.data[userID];
-          console.log('USERS', $scope.selectedUser);
+      function successCallback(response) {
+        $scope.selectedUser = response.data;
+      }
 
-        }
-        function errorCallback(response){
-          return  "Error: " + response.status + " " + response.statusText;
-        }
+      function errorCallback(response) {
+        return "Error: " + response.status + " " + response.statusText;
+      }
 
+      $scope.updateUser = function updateUser() {
+        var data = {};
+        data.name = $scope.selectedUser.name;
+        data.age = $scope.selectedUser.age;
+        data.image = $scope.selectedUser.image;
+        data.gender = $scope.selectedUser.gender;
 
+        socialService.setUser(data);
+      }
 
-      }]);
-
+    }]);
+})();
--- app/app.service.js
+++ app/app.service.js
@@ -1,12 +1,17 @@
 ;
-
+(function() {
   angular
     .module('gpApp')
-    .service('socialService', ['$http', function($http){
-        var baseURL = 'http://localhost:3000/users';
+    .service('socialService', ['$http', function($http) {
+      var baseURL = 'http://localhost:3000/user/0';
 
-        this.getUsers = function() {
-           return $http.get(baseURL);
-        };
+      this.getUsers = function() {
+        return $http.get(baseURL);
+      };
 
-      }]);
+      this.setUser = function(data) {
+        return $http.put(baseURL, data);
+      };
+
+    }]);
+})();
