effe0ae10c37327c48dc9f2047fb6e2bfe59c478
Xelen93 <253077>
2016-02-15 20:06:23 +0300

    update script validation

--- app/main.js
+++ app/main.js
@@ -1,20 +1,5 @@
-jQuery.fn.ForceNumericOnly =
-function()
-{
-    return this.each(function()
-    {
-        $(this).keydown(function(e)
-        {
-            var key = e.charCode || e.keyCode || 0;
-            // Разрешаем backspace, tab, delete, стрелки, обычные цифры и цифры на дополнительной клавиатуре
-            return (
-                key == 8 || 
-                key == 9 ||
-                key == 46 ||
-                (key >= 37 && key <= 40) ||
-                (key >= 48 && key <= 57) ||
-                (key >= 96 && key <= 105));
-        });
-    });
-};
-$("#age-person").ForceNumericOnly();
+$('#age-person').bind("change keyup input click", function() {
+    if (this.value.match(/[^0-9]/g)) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    }
+});
