d1abed1dfcf9237a5ff7a08768e15d60a508c830
k_zenchyk <k_zenchyk@wargaming.net>
2016-02-16 23:35:30 +0300

    * update gulp functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,90 @@
-var gulp = require('gulp');
-var inject = require('gulp-inject');
-var bowerFiles = require('main-bower-files');
-var uglify = require('gulp-uglify');
-var useref = require('gulp-useref');
-var gulpif = require('gulp-if');
-var minifyCss = require('gulp-minify-css');
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    changed = require('gulp-changed'),
+    rev = require('gulp-rev'),
+    browserSync = require('browser-sync'),
+    ngannotate = require('gulp-ng-annotate'),
+    del = require('del');
 
-gulp.task('default', function() {
-  // Don't touch yet
+
+gulp.task('jshint', function() {
+  return gulp.src('./app/scripts/**/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter(stylish));
+});
+
+// Clean
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+// Default task
+gulp.task('default', ['clean'], function() {
+    return gulp.start('usemin', 'imagemin','copyfonts');
+});
+
+gulp.task('usemin',['jshint'], function () {
+  return gulp.src('./app/**/*.html')
+      .pipe(usemin({
+        css:[minifycss(),rev()],
+        js: [ngannotate(),uglify(),rev()]
+      }))
+      .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('clean-gh-pages', function(){
+    return del(['./gh-pages/*']);
+})
+gulp.task('gh-pages', ['clean-gh-pages', 'clean', 'usemin', 'imagemin', 'copyfonts'], function() {
+    return gulp.src('./dist/**/*').pipe(gulp.dest('./gh-pages/'));
 })
 
-// Artyom' tasks
-// begin
-gulp.task('ag-inject', function() {
-  return gulp.src('app/index.html')
-    .pipe(inject(gulp.src(['app/**/*.js', 'app/**/*.css'])))
-    .pipe(inject(gulp.src(bowerFiles()), {
-      name: 'bower'
-    }))
-    .pipe(gulp.dest('app/'));
+// Images
+gulp.task('imagemin', function() {
+  return del(['dist/images']), gulp.src('app/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('dist/images'))
+    .pipe(notify({ message: 'Images task complete' }));
 });
 
-gulp.task('ag-compress', function() {
-  return gulp.src('app/*.html')
-    .pipe(useref({
-      searchPath: '../gp-software-travel.github.io'
-    }))
-    .pipe(gulpif('*.js', uglify({
-      mangle: false
-    })))
-    .pipe(gulpif('*.css', minifyCss()))
-    .pipe(gulp.dest('dist'))
+gulp.task('copyfonts', ['clean'], function() {
+   gulp.src('./bower_components/Materialize/dist/font/**/*.{ttf,woff,eof,svg}*')
+   .pipe(gulp.dest('./dist/font'));
 });
 
-gulp.task('ag-watch', function() {
-  return gulp.watch('app/**/*', ['compress', 'annotate'])
+// Watch
+gulp.task('watch', ['browser-sync'], function() {
+  // Watch .js files
+  gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
+      // Watch image files
+  gulp.watch('app/images/**/*', ['imagemin']);
+
 });
-// end
+
+gulp.task('browser-sync', ['default'], function () {
+    var files = [
+        'app/**/*.html',
+        'app/scripts/**/*.js',
+        'dist/**/*',
+        'bower_components/**/*',
+   ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: "dist",
+         index: "index.html"
+      }
+   });
+        // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', browserSync.reload);
+    });
+
