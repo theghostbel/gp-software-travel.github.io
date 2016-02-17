02e33be93f6ac5792eaaa0b29bdc7e410efb2059
k_zenchyk <k_zenchyk@wargaming.net>
2016-02-18 00:18:31 +0300

    * show menu

--- app/profile/profile.controller.js
+++ app/profile/profile.controller.js
@@ -46,6 +46,7 @@
           $('.collapsible').collapsible({
               accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
           });
+      $(".button-collapse").sideNav();
 
   }
 })();
